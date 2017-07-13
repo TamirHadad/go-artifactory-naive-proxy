ca9af3d78af5f7942aba5e889586186bc951eb76
tamirhadad <tamirh@jfrog.com>
2017-07-13 14:12:25 +0300

    Creating Certificates if missing

--- README.md
+++ README.md
@@ -1,6 +1,28 @@
-# go-artifactory-naive-proxy<br/>
-This project is a navie Proxy.<br/>
-For start nothing is configurable.<br/>
-Expect to find: cert.pem and key.pem next to the executable.<br/>
-HTTP port: 8099<br/>
-HTTPS port: 444<br/>
+# go-naive-proxy
+### Description
+This project is a naive Proxy, which can be used as reverse proxy for SSL authentication .<br/>
+The server redirect any request to the target address configured.
+As for now the target can only use HTTP scheme.
+### SSL Configuration
+The server will load naive_proxy_cert.pem naive_proxy_key.pem. <br/>
+If the pem files are missing they will be created.<br/>
+By default the server will start by using 8099 and 444 port.<br/>
+The Ports are configurable using the following environment variables:
+ ```
+ export PROXY_HTTP_PORT=8080
+ ```
+ ```
+ export PROXY_HTTPS_PORT=443
+ ```
+
+## Starting The Server
+By default the target of the proxy is `http://localhost:8081/`
+```
+./server
+```
+ You can change the default target by running the command with the target server as argument
+ ```
+ ./server http://127.0.0.1:8082/
+ ```
+
+
--- server/server.go
+++ server/server.go
@@ -6,17 +6,25 @@ import (
 	"path/filepath"
 	"net/http/httputil"
 	"net/url"
+	"github.com/TamirHadad/go-artifactory-naive-proxy/server/cert"
+	"os"
 )
 
-type httpResponse func (rw http.ResponseWriter, req *http.Request)
+type httpResponse func(rw http.ResponseWriter, req *http.Request)
+
 func handleHttp(reverseProxy *httputil.ReverseProxy) httpResponse {
-	return func (rw http.ResponseWriter, req *http.Request) {
-		fmt.Println("got request")
+	return func(rw http.ResponseWriter, req *http.Request) {
+		fmt.Println("*********************************************************")
+		fmt.Println("Host:    ", req.Host)
+		fmt.Println("Method:  ", req.Method)
+		fmt.Println("URI:     ", req.RequestURI)
+		fmt.Println("Agent:   ", req.UserAgent())
+		fmt.Println("*********************************************************")
 		reverseProxy.ServeHTTP(rw, req)
 	}
 }
 
-func getProxyHandler(targetUrl string) (*httputil.ReverseProxy, error){
+func getProxyHandler(targetUrl string) (*httputil.ReverseProxy, error) {
 	if len(targetUrl) == 0 {
 		targetUrl = "http://localhost:8081/"
 	}
@@ -33,29 +41,62 @@ func getProxyHandler(targetUrl string) (*httputil.ReverseProxy, error){
 		req.URL.Scheme = "http"
 	}
 
-	p := &httputil.ReverseProxy{Director: d,}
+	p := &httputil.ReverseProxy{Director: d, }
 	return p, nil
 }
 
-func main() {
-	handler, err := getProxyHandler("")
-	if err != nil {
-		panic(err)
+func getProxyTarget() string {
+	proxyTarget := ""
+	if len(os.Args) > 1 {
+		proxyTarget = os.Args[1]
 	}
-	http.HandleFunc("/", handleHttp(handler))
+	return proxyTarget
+}
+
+func startHTTPListener(handler *httputil.ReverseProxy) {
 	go func() {
-		err := http.ListenAndServe(":8099", nil)
+		// We can use the same handler for both HTTP and HTTPS
+		httpMux := http.NewServeMux()
+		httpMux.HandleFunc("/", handleHttp(handler))
+		port := "8099"
+		if httpPort := os.Getenv("PROXY_HTTP_PORT"); httpPort != "" {
+			port = httpPort
+		}
+		err := http.ListenAndServe(":" + port, httpMux)
 		if err != nil {
 			panic(err)
 		}
-		fmt.Println("Connected http")
 	}()
+}
 
-	absPathCert, _ := filepath.Abs("cert.pem")
-	absPathKey, _ := filepath.Abs("key.pem")
-	err = http.ListenAndServeTLS(":444", absPathCert, absPathKey, nil)
+func prepareHTTPSHandling(handler *httputil.ReverseProxy) (*http.ServeMux, string, string) {
+	// We can use the same handler for both HTTP and HTTPS
+	httpsMux := http.NewServeMux()
+	httpsMux.HandleFunc("/", handleHttp(handler))
+	if _, err := os.Stat(cert.CERT_FILE); os.IsNotExist(err) {
+		cert.CreateNewCert()
+	}
+	absPathCert, _ := filepath.Abs(cert.CERT_FILE)
+	absPathKey, _ := filepath.Abs(cert.KEY_FILE)
+	return httpsMux, absPathCert, absPathKey
+}
+
+func main() {
+	proxyTarget := getProxyTarget()
+	handler, err := getProxyHandler(proxyTarget)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Connected https")
+
+	// Starts a new Go routine
+	startHTTPListener(handler)
+	httpsMux, absPathCert, absPathKey := prepareHTTPSHandling(handler)
+	port := "444"
+	if httpPort := os.Getenv("PROXY_HTTPS_PORT"); httpPort != "" {
+		port = httpPort
+	}
+	err = http.ListenAndServeTLS(":" + port, absPathCert, absPathKey, httpsMux)
+	if err != nil {
+		panic(err)
+	}
 }
